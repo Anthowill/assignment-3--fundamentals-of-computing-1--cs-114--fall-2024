8a52a56ec67cf1912bf8579835f18edff4d52bcb
Anthowill <awilliams3647@gmail.com>
2024-11-11 00:03:23 -0500

    Works!

--- RunMatrix.java
+++ RunMatrix.java
@@ -13,7 +13,7 @@ public class RunMatrix {
       } while(sizeOfMatrix<=0);
 
       int[][] matrix = new int[sizeOfMatrix][sizeOfMatrix];
-      System.out.print("Matrix filled with zeros: ");
+      System.out.println("Matrix filled with zeros: ");
       printMatrix(matrix);
 
       int value = 1;
@@ -26,16 +26,31 @@ public class RunMatrix {
       System.out.println("\n Matrix populated with values: ");
       printMatrix(matrix);
 
-      for(int row=0; row < sizeOfMatrix;row++){
-        for(int colm=0; colm < sizeOfMatrix; colm++){
+      for(int row = 0; row < sizeOfMatrix; row++){
+        for(int colm = 0; colm < sizeOfMatrix - row -1; colm++){
           int inverted = matrix[row][colm];
           matrix[row][colm] = matrix[sizeOfMatrix- colm - 1][sizeOfMatrix - row -1];
           matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1] = inverted;
         }
       }
 
+      System.out.println("\n Matrix with flipped elements around the diagonal: ");
+      int size = matrix.length;
+      for(int row = 0; row < size; row++){
+        for(int colm = 0; colm<size; colm++){
+          if(row+colm ==size-1){
+            System.out.print("["+ matrix[row][colm] +"]\t");
+          }
+          else{
+            System.out.print(matrix[row][colm]+"\t");
+          }
+        }
+        System.out.println();
+      }
+
     }
-    public static void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix){
+
       for(int[] row : matrix){
         for(int value : row){
           System.out.print(value+"\t");
@@ -44,5 +59,4 @@ public class RunMatrix {
       }
     }
 }
-
 
