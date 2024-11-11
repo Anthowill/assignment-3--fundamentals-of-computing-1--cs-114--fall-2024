31494c939a608afac3bd86727775a14e5f27f608
Anthowill <awilliams3647@gmail.com>
2024-11-10 23:07:00 -0500

    Initial input taking

--- RunMatrix.java
+++ RunMatrix.java
@@ -3,4 +3,14 @@ import java.util.Scanner;
 public class RunMatrix {
   public static void main(String[] args) {
   }
+
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+
+      int sizeOfMatrix;
+      do{
+        System.out.Println("Enter a positive number for the size of the matrix: ");
+        sizeOfMatrix = input.nextInt();
+      } while(sizeOfMatrix<=0);
+    }
 }
