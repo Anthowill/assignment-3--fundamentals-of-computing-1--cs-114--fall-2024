fa3a9c30330a6d3b5241a76153dd2e4e11bc8354
Anthowill <awilliams3647@gmail.com>
2024-11-12 13:04:50 -0500

    put matrix functions into this file

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,26 @@
+import java.util.Arrays;
+
 public class Matrix {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int value : row) {
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void printMatrixWithHighlight(int[][] matrix){
+      int size = matrix.length;
+      for (int row = 0; row < size; row++) {
+          for (int colm = 0; colm < size; colm++) {
+              if (row + colm == size - 1) {
+                  System.out.print("[" + matrix[row][colm] + "]\t");
+              } else {
+                  System.out.print(matrix[row][colm] + "\t");
+              }
+          }
+          System.out.println();
+      }
+    }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,61 +2,39 @@ import java.util.Scanner;
 
 public class RunMatrix {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-      Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-      int sizeOfMatrix;
-      do{
-        System.out.println("Enter a positive number for the size of the matrix: ");
-        sizeOfMatrix = input.nextInt();
-      } while(sizeOfMatrix<=0);
+        int sizeOfMatrix;
+        do {
+            System.out.println("Enter a positive number for the size of the matrix: ");
+            sizeOfMatrix = input.nextInt();
+        } while (sizeOfMatrix <= 0);
 
-      int[][] matrix = new int[sizeOfMatrix][sizeOfMatrix];
-      System.out.println("Matrix filled with zeros: ");
-      printMatrix(matrix);
+        int[][] matrix = new int[sizeOfMatrix][sizeOfMatrix];
+        System.out.println("Matrix filled with zeros: ");
+        Matrix.printMatrix(matrix); // Use the method from MatrixUtils
 
-      int value = 1;
-      for(int row=0; row < sizeOfMatrix;row++){
-        for(int colm=0; colm < sizeOfMatrix; colm++){
-          matrix[row][colm] = value++;
+        int value = 1;
+        for (int row = 0; row < sizeOfMatrix; row++) {
+            for (int colm = 0; colm < sizeOfMatrix; colm++) {
+                matrix[row][colm] = value++;
+            }
         }
-      }
 
-      System.out.println("\n Matrix populated with values: ");
-      printMatrix(matrix);
+        System.out.println("\nMatrix populated with values: ");
+        Matrix.printMatrix(matrix); // Use the method from MatrixUtils
 
-      for(int row = 0; row < sizeOfMatrix; row++){
-        for(int colm = 0; colm < sizeOfMatrix - row -1; colm++){
-          int inverted = matrix[row][colm];
-          matrix[row][colm] = matrix[sizeOfMatrix- colm - 1][sizeOfMatrix - row -1];
-          matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1] = inverted;
+        for (int row = 0; row < sizeOfMatrix; row++) {
+            for (int colm = 0; colm < sizeOfMatrix - row - 1; colm++) {
+                int inverted = matrix[row][colm];
+                matrix[row][colm] = matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1];
+                matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1] = inverted;
+            }
         }
-      }
 
-      System.out.println("\n Matrix with flipped elements around the diagonal: ");
-      int size = matrix.length;
-      for(int row = 0; row < size; row++){
-        for(int colm = 0; colm<size; colm++){
-          if(row+colm ==size-1){
-            System.out.print("["+ matrix[row][colm] +"]\t");
-          }
-          else{
-            System.out.print(matrix[row][colm]+"\t");
-          }
-        }
-        System.out.println();
-      }
-
-    }
-    public static void printMatrix(int[][] matrix){
-
-      for(int[] row : matrix){
-        for(int value : row){
-          System.out.print(value+"\t");
-        }
-        System.out.println();
-      }
+        System.out.println("\nMatrix with flipped elements around the diagonal: ");
+        Matrix.printMatrixWithHighlight(matrix);
     }
 }
-
