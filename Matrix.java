b5979f5727d4a193b0668b20a7cd8ce0692afafe
Anthowill <awilliams3647@gmail.com>
2024-11-12 21:28:29 -0500

    Finished assignment
    Properly separated Matrix setup code into Matrix.java file and created functions for each task. Then put all the calls to the functions and system.out.print  in the RunMatrix.java file

--- Matrix.java
+++ Matrix.java
@@ -1,26 +1,54 @@
 import java.util.Arrays;
 
 public class Matrix {
-
-    public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.print(value + "\t");
-            }
-            System.out.println();
-        }
+  //to print just matrix without color
+  public static void printMatrix(int[][] matrix) {
+    for (int[] row : matrix) {
+      for (int value : row) {
+        System.out.print(value + "\t");
+      }
+      System.out.println();
     }
-    public static void printMatrixWithHighlight(int[][] matrix){
-      int size = matrix.length;
-      for (int row = 0; row < size; row++) {
-          for (int colm = 0; colm < size; colm++) {
-              if (row + colm == size - 1) {
-                  System.out.print("[" + matrix[row][colm] + "]\t");
-              } else {
-                  System.out.print(matrix[row][colm] + "\t");
-              }
-          }
-          System.out.println();
+  }
+
+  //to print matrix with color yellow diagonal
+  public static void printMatrixWithHighlight(int[][] matrix){
+    int sizeOfMatrix = matrix.length;
+
+    for (int row = 0; row < sizeOfMatrix; row++) {
+      for (int colm = 0; colm < sizeOfMatrix; colm++) {
+        if (row + colm == sizeOfMatrix - 1) {
+          System.out.print("\033[33m" + matrix[row][colm] + "\033[0m");
+        }
+        else {
+          System.out.print(matrix[row][colm] + "\t");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  //to populate matrix with number from 1 to (sizeOfMatrix * sizeOfMatrix)
+  public static void populateMatrix(int[][] matrix){
+    int value = 1, sizeOfMatrix = matrix.length;
+
+    for (int row = 0; row < sizeOfMatrix; row++) {
+      for (int colm = 0; colm < sizeOfMatrix; colm++) {
+        matrix[row][colm] = value++;
       }
     }
+  }
+
+  //to flip the matrix across the diaganol without moving the diaganol
+  public static void flipMatrixAroundDiagonal(int[][] matrix){
+    int sizeOfMatrix = matrix.length;
+
+    for (int row = 0; row < sizeOfMatrix; row++) {
+      for (int colm = 0; colm < sizeOfMatrix - row - 1; colm++) {
+        int inverted = matrix[row][colm];
+        matrix[row][colm] = matrix[sizeOfMatrix - row - 1][sizeOfMatrix - colm - 1];
+        matrix[sizeOfMatrix - row - 1][sizeOfMatrix - colm - 1] = inverted;
+      }
+    }
+  }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -3,38 +3,27 @@ import java.util.Scanner;
 public class RunMatrix {
 
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-
         int sizeOfMatrix;
+
         do {
             System.out.println("Enter a positive number for the size of the matrix: ");
             sizeOfMatrix = input.nextInt();
         } while (sizeOfMatrix <= 0);
 
         int[][] matrix = new int[sizeOfMatrix][sizeOfMatrix];
-        System.out.println("Matrix filled with zeros: ");
-        Matrix.printMatrix(matrix); // Use the method from MatrixUtils
 
-        int value = 1;
-        for (int row = 0; row < sizeOfMatrix; row++) {
-            for (int colm = 0; colm < sizeOfMatrix; colm++) {
-                matrix[row][colm] = value++;
-            }
-        }
+        System.out.println("Matrix filled with zeros: ");
+        Matrix.printMatrixWithHighlight(matrix);
 
         System.out.println("\nMatrix populated with values: ");
-        Matrix.printMatrix(matrix); // Use the method from MatrixUtils
-
-        for (int row = 0; row < sizeOfMatrix; row++) {
-            for (int colm = 0; colm < sizeOfMatrix - row - 1; colm++) {
-                int inverted = matrix[row][colm];
-                matrix[row][colm] = matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1];
-                matrix[sizeOfMatrix - colm - 1][sizeOfMatrix - row - 1] = inverted;
-            }
-        }
+        Matrix.populateMatrix(matrix);
+        Matrix.printMatrixWithHighlight(matrix);
 
         System.out.println("\nMatrix with flipped elements around the diagonal: ");
+        Matrix.flipMatrixAroundDiagonal(matrix);
         Matrix.printMatrixWithHighlight(matrix);
+
+        input.close();
     }
 }
